862177bff95829f9dc6d6878da6f91d4c8a176a9
strike9906 <ivan.klepinin00@gmail.com>
2021-01-28 05:02:48 +0200

    todo list

---
+++ todo-mini (react)/src/Todo/TodoList.js
@@ -0,0 +1,28 @@
+import TodoItem from "./Todoitem"
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const styles = {
+    ul: {
+        listStyle: 'none',
+        margin: 0,
+        padding: 0
+    }
+}
+
+function TodoList(props) {
+    return (
+        <ul style={styles.ul}>
+            {props.todos.map((todo, index) => {
+                return <TodoItem todo ={todo} key={todo.id} index={index} onChange={props.onToggle}/>
+            })}
+        </ul>
+    )
+}
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onToggle: PropTypes.func.isRequired
+}
+
+export default TodoList
